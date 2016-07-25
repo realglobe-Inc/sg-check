cc9c4942b4625755379e5c6e8e3b29b0503e4542
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-25 17:09:39 +0900

    Update deps

--- ci/browser.js
+++ ci/browser.js
@@ -9,12 +9,12 @@
 process.chdir(`${__dirname}/..`)
 
 const apeTasking = require('ape-tasking')
-const apeCompiling = require('ape-compiling')
+const ababelES2015 = require('ababel-es2015')
 const filedel = require('filedel')
 
 apeTasking.runTasks('browser', [
   () => filedel('sims/browser/**/*.js'),
-  () => apeCompiling.compileToEs5('**/*.js', {
+  () => ababelES2015('**/*.js', {
     cwd: 'lib',
     out: 'sims/browser'
   })
